71406d0affd25695b957f7dfc4e44774919c8994
Garrett <gart@mangostudios.uk>
2023-04-22 10:45:58 -0400

    change port

--- index.ts
+++ index.ts
@@ -13,6 +13,6 @@ app.get("/bundle.js", (req, res) => {
     res.sendFile(path.resolve("./dist/bundle.js"));
 })
 
-app.listen(3002, () => {
-    console.log("Listening on port 3002");
+app.listen(3017, () => {
+    console.log("Listening on port 3017");
 });
